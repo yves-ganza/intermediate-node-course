0d88bd1cec96af10cb5b0384c8989d268df47237
lebonYG <lebonganza@outlook.com>
2021-01-29 15:09:23 -0500

    add landing page

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express= require('express');
 const mongoose= require('mongoose');
 const bodyParser= require('body-parser');
-const User = require('./models/User')
+const bcrypt = require('bcrypt');
+const saltRounds = 10
+const User = require('./models/User');
 const port=8000;
 const app= express();
 
-mongoose.connect('mongodb://localhost/UserData', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb://localhost/UserData', {useNewUrlParser: true, useUnifiedTopology: true});
 
+app.use(express.static(__dirname + '/public'))
 app.use(bodyParser.json());
+
 
 app.listen(port, ()=>{
 	console.log(`server is listening on port:${port}`)
@@ -24,14 +28,23 @@ function sendResponse(res, err, data){
     res.json({success: true, data: data})
   }
 }
-
+app.get('/',(req, res)=>{
+  res.sendFile(__dirname + '/views/index.html')
+})
 app.post('/users',(req,res)=>{
-  User.create({
-    name: req.body.newData.name,
-    email: req.body.newData.email,
-    password: req.body.newData.password
-  },
-  (err, data)=>{sendResponse(res, err, data)})
+  bcrypt.hash(req.body.newData.password, saltRounds, (err, hash)=>{
+    if(err){
+      res.json({success: false, message: "Enter valid password"})
+    }
+    else{
+      User.create({
+        name: req.body.newData.name,
+        email: req.body.newData.email,
+        password: hash
+      },
+      (err, data)=>{sendResponse(res, err, data)})
+    }
+  })
 })
 
 app.route('/users/:id')
