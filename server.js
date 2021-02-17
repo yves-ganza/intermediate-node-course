065b9c70d3e1cc68b648848b25063613359097b6
lebonYG <lebonganza@outlook.com>
2021-02-16 23:07:59 -0500

    Add more views and style

--- server.js
+++ server.js
@@ -2,16 +2,13 @@ require('dotenv').config()
 const express = require('express')
 const session = require('express-session')
 const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const bcrypt = require('bcrypt')
-const saltRounds = 10
-const User = require('./models/User')
 const passport = require('passport')
-const LocalStrategy = require('passport-local')
 const app = express()
-const myDB = require('./connection')
-const { ObjectID } = require('mongodb')
+const routes = require('./routes')
+const auth = require('./auth')
 
+app.set('view engine', 'pug')
+app.set('views', __dirname+'/views/')
 app.use(express.static(__dirname + '/public'))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
@@ -37,147 +34,13 @@ function sendResponse(res, err, data) {
   }
 }
 
-function ensureAuthenticated(req, res, next){
-  if(req.isAuthenticated()){
-    return next()
-  }
-  return res.redirect('/login')
-}
-
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(
   ()=>{
-    app.route('/')
-    .get(ensureAuthenticated,(req,res)=>{
-      res.sendFile(__dirname + '/views/index.html')
-    })
-
-    app.route('/login')
-    .get((req, res) => {
-      res.sendFile(__dirname + '/views/login.html')
-    })
-    .post(passport.authenticate('local', {failureRedirect: '/login'}),(req, res)=>{
-      console.log('Logged in')
-      res.redirect('/')
-    })
-
-    app.route('/register').post((req, res, next)=>{
-      console.log('Registering...')
-      User.findOne({username: req.body.username}, (err, user)=>{
-        if(err){
-          console.log(err)
-          next(err)
-        }
-        else if(user){
-          console.log('User already registered')
-          res.redirect('/')
-        }
-        else{
-          const hash = bcrypt.hashSync(req.body.password, 12)
-          User.create({
-            username: req.body.username,
-            password: hash
-          }, (err, user)=>{
-            console.log('h1')
-            if(err){
-              res.redirect('/')
-            }
-            else if(!user){
-              res.redirect('/')
-            }
-            else{
-              console.log('User registered')
-              next(null, user)
-            }
-          })
-        }
-      })
-    }, passport.authenticate('local', {failureRedirect: '/login'}), (req, res)=>{
-      res.redirect('/')
-    })
-
-    app.post('/users', (req, res) => {
-      User.create(
-        {
-          name: req.body.newData.name,
-          email: req.body.newData.email,
-          password: req.body.newData.password,
-        },
-        (err, data) => {
-          sendResponse(res, err, data)
-        }
-      )
-      /** 
-      bcrypt.hash(req.body.newData.password, saltRounds, (err, hash) => {
-        if (err) {
-          res.json({ success: false, message: 'Enter valid password' })
-        } else {
-          
-        }
-      })*/
-    })
-
-    app.route('/profile').get(ensureAuthenticated, (req, res)=>{
-      res.sendFile(__dirname+'/views/index.html');
-    })
-  
-    app
-      .route('/users/:id')
-      .get((req, res) => {
-        User.findById(req.params.id, (err, data) => {
-          sendResponse(res, err, data)
-        })
-      })
-      .put((req, res) => {
-        User.findByIdAndUpdate(
-          req.params.id,
-          {
-            name: req.body.newData.name,
-            email: req.body.newData.email,
-            password: req.body.newData.password,
-          },
-          { new: true },
-          (err, data) => {
-            sendResponse(res, err, data)
-          }
-        )
-      })
-      .delete((req, res) => {
-        User.findByIdAndDelete(req.params.id, (err, data) => {
-          sendResponse(res, err, data)
-        })
-      })
-
-      passport.serializeUser((user, done) => {
-        done(null, user._id)
-      })
-    
-      passport.deserializeUser((id, done) => {
-        User.findOne({ _id: new ObjectID(id) }, (err, user) => {
-          done(null, user)
-        })
-      })
-    
-      passport.use(
-        new LocalStrategy((username, password, done) => {
-          User.findOne({ username: username }, (err, user) => {
-            console.log(`User ${username} attempted to login`)
-            if (err) {
-              console.log(err)
-              return done(err)
-            }
-            if (!user) {
-              return done(null, false)
-            }
-            if (!bcrypt.compareSync(password, user.password)) {
-              return done(null, false)
-            }
-            return done(null, user)
-          })
-        })
-      )
+    routes(app)
+    auth(app)
   },
   err=>{
     app.get('/', (req, res)=>{
